15227ba795a17890a19c1577d45d3ca6bf1f3757
MsCurly <zuleyha_98@live.com>
2021-01-17 15:27:15 +0300

    ilk gonderme

---
+++ char.IsLetter()/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @char.IsLetter__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool res;
+            char deger = '2';
+            Console.WriteLine("Değer = " + deger);
+            res = Char.IsLetter(deger);
+            Console.WriteLine("Girilen değer harf mi? = " + res);
+
+
+            Console.ReadLine();
+        }
+    }
+    }
+}
